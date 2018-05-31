4face7a5fc98131b8b6435003dfe6fc5aeee7875
sandiaY <sandiasangrienta@gmail.com>
2018-05-31 12:12:46 -0500

    one

--- conexion.php
+++ conexion.php
@@ -1,11 +1,21 @@
 <?php
+function conectarBD()
+{
+$host="ec2-75-101-142-91.compute-1.amazonaws.com";
+$port="5432";
+$dbname="dcf9b29vh3lker";
+$user="hrwgjsttrwmdyc";
+$password="e2a3791fe011ae8d213316428a5f52e6ebc1bdec56c343eeb3591cc83b831091";
+$db=pg_connect("$host $port $dbname $user $password");
 
-
-$con= pg_connect("host='localhost' dbname=productos port=8085 user=postgres password=olakease") or die ("Error de Conexion".pg_last_error());
-// Conectamos con la base de datos
-
+if(!$db)
+{
+  echo 'Error:'.pg_last_error;
+}
+else
+{
+ echo '<h3>Conexion exitosa </h3>';
+ return $bd; 
+}
+}
 ?>
-
-
-
-
